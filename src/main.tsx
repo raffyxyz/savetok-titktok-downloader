33747135ad7b76e58eb51a58a566f6746b33a4e8
unknown <raffy.amoguis.projects@gmail.com>
2023-02-11 21:17:40 +0800

    remove script

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { registerSW } from 'virtual:pwa-register';
-
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
