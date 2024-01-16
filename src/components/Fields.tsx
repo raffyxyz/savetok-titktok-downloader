f4502b1abaa86fb8b0ed10a8f9ebfcd6facecfaa
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-01-16 10:35:51 +0800

    update: added clear action in url field

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Group, TextInput, Button } from '@mantine/core';
-import { IconLink } from '@tabler/icons';
-import { useForm } from '@mantine/form';
-import { useVideo } from '../store/videoStore';
-import Result from './Result';
+import React from "react";
+import { Group, TextInput, Button, ActionIcon } from "@mantine/core";
+import { IconLink, IconX } from "@tabler/icons";
+import { useForm } from "@mantine/form";
+import { useVideo } from "../store/videoStore";
+import Result from "./Result";
 
 const Fields: React.FC = () => {
   const video = useVideo((state) => state.video);
@@ -14,39 +14,50 @@ const Fields: React.FC = () => {
   //use form validation
   const form = useForm({
     initialValues: {
-      link: '',
+      link: "",
     },
 
     validate: {
       link: (value) =>
         value.length === 0
-          ? 'Link should not be empty'
+          ? "Link should not be empty"
           : /^(ftp|http|https):\/\/[^ "]+$/.test(value)
           ? null
-          : 'Input is not a link',
+          : "Input is not a link",
     },
   });
+
+  function handleClearInputBox() {
+    form.reset();
+  }
   return (
     <>
-      <div style={{ marginTop: '4em' }}>
+      <div style={{ marginTop: "4em" }}>
         <form
           onSubmit={form.onSubmit((values) => fetchVideo(values.link))}
-          autoComplete='off'
+          autoComplete="off"
         >
-          <Group position='center' spacing='xs' align='flex-start'>
+          <Group position="center" spacing="xs" align="flex-start">
             <TextInput
-              placeholder='Paste link here'
-              size='lg'
-              sx={{ width: '600px' }}
+              placeholder="Paste link here"
+              size="lg"
+              sx={{ width: "600px" }}
               icon={<IconLink size={14} />}
-              {...form.getInputProps('link')}
+              rightSection={
+                form.values.link !== "" && (
+                  <ActionIcon variant="transparent">
+                    <IconX size={14} onClick={handleClearInputBox} />
+                  </ActionIcon>
+                )
+              }
+              {...form.getInputProps("link")}
             />
             <Button
-              size='lg'
-              color='grape'
+              size="lg"
+              color="grape"
               loading={loading}
-              loaderPosition='right'
-              type='submit'
+              loaderPosition="right"
+              type="submit"
             >
               Get Video
             </Button>
